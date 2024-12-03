aa826de875022692896bf4038582485236d7f497
Djordje Uskokovic <djordje.uskokovic@link.co.rs>
2024-12-03 20:16:51 +0100

    izmenjen test

--- vremenska_prognoza.py
+++ vremenska_prognoza.py
@@ -1,5 +1,6 @@
 temeperatura = int(input("Unesite temepraturu:"))
-test_temeperatura = -1 
+test_temeperatura = -1
+test_temeperatura = 35 
 temeperatura = test_temeperatura
 poruka = ""
 if temeperatura < 0:
@@ -11,6 +12,6 @@ if temeperatura > 0 :
         poruka = "Ukljucite klima uredjaje"
 
 
-ocekivana_poruka = "Oprez klizavo"
+ocekivana_poruka = "Ukljcuite klima uredjaj"
 if poruka == ocekivana_poruka:
     print("Case-ispod nule - Test prosao")
