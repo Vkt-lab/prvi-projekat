059f2172e5ea1457bba44f7804a39ea736cad3a1
Djordje Uskokovic <djordje.uskokovic@link.co.rs>
2024-12-03 19:46:36 +0100

    dodat prvi fajl

---
+++ vremenska_prognoza.py
@@ -0,0 +1,11 @@
+temeperatura = int(input("Unesite temepraturu:"))
+
+if temeperatura < 0:
+    print("Oprez klizavo")
+
+if temeperatura > 0 :
+    print("Temperatur iznad 0")
+    if temeperatura > 30:
+        print("Ukljucite klima uredjaje")
+
+        
